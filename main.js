cb8770022879ca9190d23074c0a321f596766463
PurvaYadav <94224975+PurvaYadav@users.noreply.github.com>
2022-03-29 18:30:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,38 @@ function preload() {
 function setup(){
 canvas = createCanvas(400,400);
 canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+
+
 }
 
-function draw(){
-
+function draw()
+{
+    image(video,0,0,400,400);
+        
 }
 
 function take_snapshot(){
 save("myLipsFilter.jpg");
 }
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialised');
+    }
+    
+    function gotPoses(results) 
+    {
+        if(results.length = 0) 
+        {
+            console.log(results);
+            console.log(results = "lips x = " + results[0].pose.lips.x);
+            console.log(results = "lips y = " + results[0].pose.lips.y);
+        }
+    
+    } 
+    
